3aef747983d569d5c155eaa181fb427a33407272
Alexandre Ouellet <alexandre.ouellet67@hotmail.com>
2024-03-25 15:41:56 -0400

    Situation de départ

---
+++ RepartitionLocaux/ControleurAssignationLocaux.cs
@@ -0,0 +1,70 @@
+﻿namespace RepartitionLocaux
+{
+    /// <summary>
+    /// Principale classe du système responsable de résoudre les contraintes entre les affectations des cours à différents locaux et à différentes
+    /// périodes.
+    /// </summary>
+    internal class ControleurAssignationLocaux
+    {
+        /// <summary>
+        /// Liste des locaux disponibles dans l'établissement.
+        /// </summary>
+        public Local[] Locaux { get; private set; }
+
+        /// <summary>
+        /// Liste des cours qui se donnent dans l'établissement.
+        /// </summary>
+        public Cours[] Cours { get; private set; }
+
+        /// <summary>
+        /// Crée un nouveau contrôleur de l'assignation des locaux.
+        /// </summary>
+        /// <param name="locaux">La liste des locaux de l'établissement.</param>
+        /// <param name="cours">La liste des cours de l'établissement.</param>
+        ControleurAssignationLocaux(Local[] locaux, Cours[] cours)
+        {
+            Locaux = locaux;
+            Cours = cours;
+        }
+
+        /// <summary>
+        /// Détermine si les deux périodes admettent une intersection ou non.
+        /// </summary>
+        /// <param name="periode1">La première période dans la vérification.</param>
+        /// <param name="periode2">La seconde période dans la vérification.</param>
+        /// <returns>true si les deux périodes admettent une intersection, false autrement.</returns>
+        private bool PeriodesIntersectent(Periode periode1, Periode periode2)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Cherche un local de disponible à la période indiquée. La valeur nulle est retournée si aucun local n'est disponible.
+        /// </summary>
+        /// <param name="periode">La période dans laquelle le local doit être disponible.</param>
+        /// <returns>Le local qui est disponible à la période indiquée ou null si aucun local n'est disponible.</returns>
+        private Local TrouverLocal(Periode periode)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Affecte chaque cours à un local de sorte qu'il n'y ait pas deux cours qui se donnent en même temps dans le même local.
+        /// </summary>
+        public void AffecterLocaux()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Point d'entrée du programme.
+        /// </summary>
+        /// <param name="args">Arguments passés au programme.</param>
+        public static void Main(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
---
+++ RepartitionLocaux/Local.cs
@@ -0,0 +1,22 @@
+﻿namespace RepartitionLocaux
+{
+    /// <summary>
+    /// Représente une salle de classe dans laquelle peut se tenir un cours.
+    /// </summary>
+    internal class Local
+    {
+        /// <summary>
+        /// Code qui identifie le local de façon unique.
+        /// </summary>
+        public string Code { get; private set; }
+
+        /// <summary>
+        /// Crée un nouveau local avec uniquement un code unique.
+        /// </summary>
+        /// <param name="code">Le code unique au local.</param>
+        public Local(string code)
+        {
+            Code = code;
+        }
+    }
+}
